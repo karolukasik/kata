fcf5b84ae5e9a767feacb6eb72989eb1f93f7f79
karolukasik <karo.lukasik@gmail.com>
2022-12-26 13:53:07 +0100

    Added completed solution for FizzBuzz

--- FizzBuzz/src/main/java/rocks/kata/FizzBuzz.java
+++ FizzBuzz/src/main/java/rocks/kata/FizzBuzz.java
@@ -2,17 +2,24 @@ package rocks.kata;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        
+        FizzBuzz fizz = new FizzBuzz();
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizz.returnFizzBuzzOrNumber(i));
+        }
     }
 
-    public String returnFizzBuzzOrNumber(int index) {
+    public String returnFizzBuzzOrNumber(int number) {
         StringBuilder toReturn = new StringBuilder();
-        if(index % 3 == 0){
+        if (number % 3 == 0) {
             toReturn.append("Fizz");
+        }
+        if (number % 5 == 0) {
+            toReturn.append("Buzz");
+        }
+        if (toReturn.length() == 0) {
+            toReturn.append(number);
         }
         return toReturn.toString();
     }
-
-    
 
 }
--- FizzBuzz/src/test/java/rocks/kata/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/rocks/kata/FizzBuzzTest.java
@@ -1,11 +1,8 @@
 package rocks.kata;
 
-
 import static org.junit.Assert.assertTrue;
 
-
 import org.junit.Test;
-
 
 public class FizzBuzzTest {
 
@@ -13,15 +10,38 @@ public class FizzBuzzTest {
 
     @Test
     public void ifDividedByThreeReturnFizz() {
-        String checkNumber6 = fizz.returnFizzBuzzOrNumber(6);
-        String checkNumber9 = fizz.returnFizzBuzzOrNumber(9);
+        String checkNumberSix = fizz.returnFizzBuzzOrNumber(6);
+        String checkNumberNine = fizz.returnFizzBuzzOrNumber(9);
 
-        assertTrue(checkNumber6.equals("Fizz"));
-        assertTrue(checkNumber9.equals("Fizz"));
-
+        assertTrue(checkNumberSix.equals("Fizz"));
+        assertTrue(checkNumberNine.equals("Fizz"));
     }
-    //if multiple of 5, return Buzz
-    //if multiple of 15, return FizzBuzz
-    //if not multiple by 3 or 5, return number
+
+    @Test
+    public void ifDivisibleByFiveReturnBuzz() {
+        String checkNumberFive = fizz.returnFizzBuzzOrNumber(5);
+        String checkNumberTen = fizz.returnFizzBuzzOrNumber(10);
+
+        assertTrue(checkNumberFive.equals("Buzz"));
+        assertTrue(checkNumberTen.equals("Buzz"));
+    }
+
+    @Test
+    public void ifDivisibleByFifteenReturnFizzBuzz() {
+        String checkNumberFifteen = fizz.returnFizzBuzzOrNumber(15);
+        String checkNumberFortyFive = fizz.returnFizzBuzzOrNumber(45);
+
+        assertTrue(checkNumberFifteen.equals("FizzBuzz"));
+        assertTrue(checkNumberFortyFive.equals("FizzBuzz"));
+    }
     
+    @Test
+    public void ifNotDivisibleByThreeOrFiveReturnNumber() {
+        String checkNumberTwo = fizz.returnFizzBuzzOrNumber(2);
+        String checkNumberNineteen = fizz.returnFizzBuzzOrNumber(19);
+    
+        assertTrue(checkNumberTwo.equals("2"));
+        assertTrue(checkNumberNineteen.equals("19"));
+    }
+
 }

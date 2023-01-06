5498962f10805df96f88f5ce05876eaec81de86f
karolukasik <karo.lukasik@gmail.com>
2023-01-06 21:12:16 +0100

    added FizzBuzzLauncher implementation to print lines in terminal

--- FizzBuzz/src/main/java/rocks/kata/FizzBuzzLauncher.java
+++ FizzBuzz/src/main/java/rocks/kata/FizzBuzzLauncher.java
@@ -1,7 +1,20 @@
 package rocks.kata;
 
-public class FizzBuzzLauncher{
+public class FizzBuzzLauncher {
     public static void main(String[] args) {
-        System.out.println("Hello launcher");
+        FizzBuzz[] fizzs = { new FizzBuzzLiterals(), new FizzBuzzPattern(), new FizzBuzzStringBuilder() };
+
+        for (FizzBuzz fizz : fizzs) {
+            System.out.println(fizz.getClass().getSimpleName());
+            printFizzBuzzInLines(fizz, 100);
+            System.out.println("---");
+        }
+
+    }
+
+    private static void printFizzBuzzInLines(FizzBuzz fizz, int numberOfLines) {
+        for (int i = 1; i <= numberOfLines; i++) {
+            System.out.println(fizz.returnFizzBuzzOrNumber(i));
+        }
     }
 }
